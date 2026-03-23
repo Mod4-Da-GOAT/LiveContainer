762cd2f67185acc32dca9ae08e8d641aa5040e62
Mod4 <omarelfarok24135@gmail.com>
2026-03-23 23:53:57 +0400

    Update utils.m

--- TweakLoader/utils.m
+++ TweakLoader/utils.m
@@ -1,11 +1,9 @@
 #import "utils.h"
 
-void swizzleClassMethod(Class cls, SEL origSel, SEL swizzledSel) {
-    Method origMethod = class_getClassMethod(cls, origSel);
-    Method swizzledMethod = class_getClassMethod(cls, swizzledSel);
-    method_exchangeImplementations(origMethod, swizzledMethod);
+void swizzle(Class class, SEL originalAction, SEL swizzledAction) {
+    method_exchangeImplementations(class_getInstanceMethod(class, originalAction), class_getInstanceMethod(class, swizzledAction));
 }
 
 void swizzleClassMethod(Class class, SEL originalAction, SEL swizzledAction) {
     method_exchangeImplementations(class_getClassMethod(class, originalAction), class_getClassMethod(class, swizzledAction));
-}
+}'
