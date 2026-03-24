6311342f4cff71f89f7f4899384b8446311d1a92
Mod4 <omarelfarok24135@gmail.com>
2026-03-24 22:40:48 +0400

    Update LCSettingsView.swift

--- LiveContainerSwiftUI/Views/Settings/LCSettingsView.swift
+++ LiveContainerSwiftUI/Views/Settings/LCSettingsView.swift
@@ -262,6 +262,15 @@ struct LCSettingsView: View {
                     } label: {
                         Text("lc.settings.jitEnabler".loc)
                     }
+                    
+                    // DEBUGGER STATUS INDICATOR
+                    HStack {
+                        Text("Debugger Attached")
+                        Spacer()
+                        Circle()
+                            .fill(isDebuggerAttached() ? Color.green : Color.red)
+                            .frame(width: 12, height: 12)
+                    }
 
                 } header: {
                     Text("JIT")
@@ -560,6 +569,33 @@ struct LCSettingsView: View {
     func openForkedRepo() {
         UIApplication.shared.open(URL(string: "https://github.com/Mod4-Da-GOAT/LiveContainer")!)
     }
+    
+    func isDebuggerAttached() -> Bool {
+        // Method 1: Check P_TRACED flag (works for lldb, etc)
+        var info = kinfo_proc()
+        var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
+        var size = MemoryLayout<kinfo_proc>.stride
+
+        let result = sysctl(&mib, UInt32(mib.count), &info, &size, nil, 0)
+
+        if result == 0 && (info.kp_proc.p_flag & P_TRACED) != 0 {
+            return true
+        }
+
+        // Method 2: Check for JIT-enabled memory (works for StikDebug)
+        // If we can allocate executable memory, JIT is enabled
+        let testSize = 1024
+        let ptr = mmap(nil, testSize, PROT_READ | PROT_WRITE | PROT_EXEC, 
+                      MAP_PRIVATE | MAP_ANONYMOUS | MAP_JIT, -1, 0)
+
+        if ptr != MAP_FAILED {
+            munmap(ptr, testSize)
+            return true
+        }
+
+        return false
+    }
+
     
     func export() {
         let fileManager = FileManager.default
